ea60cec0e5f8b0de2fd40cfe1c7d4372fca3a1a6
Dina Alaa <dina.3laa93@gmail.com>
2021-09-22 00:05:08 +0200

    Stylesh Success login page

--- success.php
+++ success.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="fb_login.css">
 </head>
 <body>
-    <h1>مرحبا بك في شبكه التواصل الاجتماعي فيس بوك </h1>
-    <h2>بريدك الالكتروني هو :<?php echo $email ?></h2>
+    <h1 class="welcome_msg">مرحبا بك في شبكه التواصل الاجتماعي فيس بوك </h1>
+    <h2 id="your_mail_is">بريدك الالكتروني هو :<?php echo $email ?></h2>
 </body>
 </html>
